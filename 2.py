95705bf50dd594f431259a7bdc3c15d9ad0b289f
Ivan Kinder <ivankinder11@gmail.com>
2020-04-04 18:44:37 +0300

    Второе задание

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
 from Class_2 import Road
 
-
+road = Road(5000, 20)
+road.how_mass()
--- Class_2.py
+++ Class_2.py
@@ -1,4 +1,7 @@
 class Road:
-    def __init__(self):
-        _length = ''
-        _width = ''
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def how_mass(self):
+        print(self._length * self._width * 0.025 * 5, " тонн")
